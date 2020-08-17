568b95ebf79e74e79739d0a57d5e4379387d4836
tuppi-ovh <tuppi-ovh@gmail.com>
2020-08-17 23:04:34 +0200

    add echo plugin

---
+++ plugins/echo/__init__.py
@@ -0,0 +1,51 @@
+"""
+Data Server PI - high level application for the Smart Home data acquisition.
+Copyright (C) 2020 Vadim MUKHTAROV
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+For information on Data Server PI: tuppi.ovh@gmail.com
+"""
+
+import os
+import sys
+
+
+# constants
+COMMANDS = [{"command": "echo.<message.with.points>", "description": ""}]
+
+# config
+config_www = None
+
+
+def handle(command):
+    """ Handles echo command.
+    """
+    retval = []
+    # about program
+    if command.find("echo.") != -1:
+         text = command[5:len(command)]
+        retval.append({"text": text})
+    # unknown command
+    else:
+        pass
+    # return
+    return retval
+
+
+def get_commands():
+    """ Returns a list of all supporteed commands.
+    """
+    return COMMANDS
+
